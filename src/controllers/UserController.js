95517c4bf37de5c70454f89c89e3a5bdfa060ef7
cesar-machado <cesarmach19@gmail.com>
2020-04-27 18:25:47 -0300

    Fazendo a criptografia da senha e colocando o crud

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,6 @@
 const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+
 
 module.exports = {
     async index(req,res) {
@@ -10,8 +12,25 @@ module.exports = {
     async store(req, res) {
         const{name, password} = req.body;
 
-        const user = await User.create({ name, password});
+        //hash
+         const salt = await bcrypt.genSalt(10);
+         const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+        //Create User
+         const user = await User.create({ name, password: hashPassword});
         return res.json(user)
-    }    
+    },
+
+    async delete(req, res) {
+        const user = await User.findByPk(req.params.id)
+            .then((user) => {
+                return user.destroy();
+            })
+
+
+        return res.json(user)
+    }
+    
+    
 
 }
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,9 @@ const UserController = require('./controllers/UserController')
 const routes =  express.Router();
 
 routes.get('/users', UserController.index)
-routes.post('/users', UserController.store)
+routes.post('/register', UserController.store)
+routes.delete('/users/:id', UserController.delete)
+
 
 
 module.exports = routes
